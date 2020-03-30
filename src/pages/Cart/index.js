38b73d977008cf33b37f35e5b1b63d28e577efc9
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-30 19:20:04 -0300

    Cart page style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,67 @@
 import React from 'react';
 
+import { FiPlusCircle, FiMinusCircle, FiXCircle } from 'react-icons/fi'
+
 import './styles.css';
 
 export default function Cart() {
   return (
-    <h1>Cart</h1>
+    <main className="container">
+      <div className="bag-container">
+        <table className="book-table">
+          <thead>
+            <tr>
+              <th />
+              <th>Livro</th>
+              <th>Quantidade</th>
+              <th>Subtotal</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <td>
+                  <img src="https://images-na.ssl-images-amazon.com/images/I/51w53T12s8L.jpg" alt="JavaScript: O Guia Definitivo" />
+                </td>
+                <td>
+                  <strong>JavaScript: O Guia Definitivo</strong>
+                  <span>R$ 146,08</span>
+                </td>
+                <td>
+                  <div>
+                    <button type="button" onClick={() => {}}>
+                      <FiMinusCircle size={20} color="#33BFCB" />
+                    </button>
+                    <input type="number" readOnly value="1" />
+                    <button type="button" onClick={() => {}}>
+                      <FiPlusCircle size={20} color="#33BFCB" />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>R$ 1000,00</strong>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    <FiXCircle size={20} color="#33BFCB" />
+                  </button>
+                </td>
+              </tr>
+          </tbody>
+        </table>
+
+        <footer>
+          <button type="button">Encomendar</button>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>R$ 1000,00</strong>
+          </div>
+        </footer>
+      </div>
+    </main>
   );
 }
